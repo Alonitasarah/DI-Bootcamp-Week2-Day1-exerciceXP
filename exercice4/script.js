3d0dbdd1b0f70a7fc74996e05e7db68877ef04c8
Alonitasarah <121178294+Alonitasarah@users.noreply.github.com>
2023-01-06 10:52:47 +0000

    Add files via upload

---
+++ exercice4/script.js
@@ -0,0 +1,16 @@
+let c;
+    let a = 34;
+    let b = 21;
+
+    console.log(a+b) //first expression
+    // Prediction:It will output 55, because 34 and 21 are numbers
+    // Actual:55
+
+    a = 2;
+
+    console.log(a+b) //second expression
+    // Prediction:It will output 23, because 2 and 21 are numbers
+    // Actual:23
+    console.log(3 + 4 + '5');
+    // Prediction:on ne peut pas additionné un nombre et un string,on ne peut que les concaténé
+    // Actual:75
