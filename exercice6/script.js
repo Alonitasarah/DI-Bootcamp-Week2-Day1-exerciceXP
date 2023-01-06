bc992557251771c2470f53107e104b0b9891f697
Alonitasarah <121178294+Alonitasarah@users.noreply.github.com>
2023-01-06 11:17:14 +0000

    Add files via upload

---
+++ exercice6/script.js
@@ -0,0 +1,55 @@
+5 + "34"
+// Prediction:c'est une concaténation
+// Actual:534
+
+5 - "4"
+// Prediction:c'est 5 - la valeur 4
+// Actual:1
+
+10 % 5
+// Prediction:cest la division 
+// Actual:2
+
+5 % 10
+// Prediction:c'est une division
+// Actual:0.5
+
+"Java" + "Script"
+// Prediction:c'est la concaténation
+// Actual:javascript
+
+" " + " "
+// Prediction:c'est concaténation 
+// Actual:""
+
+" " + 0
+// Prediction:c'est la concaténation
+// Actual:"0"
+
+true + true
+// Prediction:true à pour valeur 1 
+// Actual:2
+
+true + false
+// Prediction:true = 1 et false = 0
+// Actual:1
+
+false + true
+// Prediction:true = 1 et false = 0
+// Actual:
+
+false - true
+// Prediction:true = 1 et false = 0
+// Actual:-1
+
+!true
+// Prediction:quand on met point d'esclamation ca veut dire le contraire de ce qu'on veut afficher
+// Actual:false
+
+3 - 4
+// Prediction:c'est la soustraction
+// Actual:-1
+
+"Bob" - "bill"
+// Prediction:on peut pas faire une concatenation avec la soustraction
+// Actual:NaN
